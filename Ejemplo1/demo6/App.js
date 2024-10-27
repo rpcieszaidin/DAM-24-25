bb28b07ddd42fdaeb0ed9fe76cb8a1f509564fa0
rpcieszaidin <rauljimenez@ieszaidinvergeles.org>
2024-10-27 23:07:29 +0100

    Ejemplo1

--- Ejemplo1/demo6/App.js
+++ Ejemplo1/demo6/App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
+import HomeScreen2 from './src/screens/HomeScreen2';
+import HomeScreen3 from './src/screens/HomeScreen3';
+import HomeScreen4 from './src/screens/HomeScreen4';
 
 const lista = {
   carLists : [
@@ -21,6 +24,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <HomeScreen data={lista} text="Lista de elementos" />
+      <HomeScreen2 data={lista} text="Lista de elementos" />
+      <HomeScreen3 data={lista} text="Lista de elementos" />
+      <HomeScreen4 data={lista} text="Lista de elementos" />
       <StatusBar style="auto" />
     </View>
   );
