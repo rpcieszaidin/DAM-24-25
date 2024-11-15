97618c187a1819d88a8634e95a874c1d08a4f840
rpcieszaidin <114917189+rpcieszaidin@users.noreply.github.com>
2024-11-15 11:55:32 +0100

    Trabajando sobre paginación

---
+++ Ejemplo6/demo6/src/components/Slider.tsx
@@ -0,0 +1,37 @@
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import { Movie } from '../config/entities/Movie'
+
+interface Movies {
+  movies: Movie[];
+  height: number;
+}
+
+export default function Slider({ movies, height }: Movies) {
+  return (
+    <View>
+      <ScrollView style = {styles.contenedor} horizontal={true}>
+        {movies.map((item) => (
+          <Image style = {styles.imagen} key = {item.id}
+            source={{
+              uri: `https://image.tmdb.org/t/p/original${item.poster}`,
+            }}
+          />
+        ))}
+
+      </ScrollView>
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  contenedor: {
+    height: 300,
+  }, 
+  imagen: {
+    width: 200,
+    //height: 200,
+    margin: 1
+  }
+})
